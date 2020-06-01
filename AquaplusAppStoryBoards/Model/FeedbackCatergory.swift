bbaa5f7e869cdecb10d5ddff19bce29f31a4c227
David <dwmendesdasilva@gmail.com>
2020-06-01 23:51:41 +0100

    Added FeedbackViewController

---
+++ AquaplusAppStoryBoards/Model/FeedbackCatergory.swift
@@ -0,0 +1,16 @@
+//
+//  FeedbackCatergory.swift
+//  AquaplusAppStoryBoards
+//
+//  Created by David Mendes Da Silva on 01/06/2020.
+//  Copyright © 2020 David Mendes Da Silva. All rights reserved.
+//
+
+import Foundation
+
+enum FeedbackCatergory: String, CaseIterable {
+    case eventFeedback = "Event Feedback"
+    case ticketEnquiry = "Ticket enquiry"
+    case bug = "Report a bug"
+    case featureRequest = "Featur request"
+}
